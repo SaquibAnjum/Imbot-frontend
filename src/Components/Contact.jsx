c566c6cd67b42e98674754f6a9323fc997d07f77
SaquibAnjum <saquib1312@gmail.com>
2025-08-17 15:42:15 +0530

    Updated project with latest changes

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -9,25 +9,25 @@ const Contact = () => {
         Contact
       </h2>
 
-      <p className='text-center text-gray-500'>For any inquiries or support requests, feel free to reach out to us at <a href="mailto:shahareyar2003@gmail.com" className='text-white'>shahareyar2003@gmail.com</a>. We will get back to you as soon as possible.</p>
+      <p className='text-center text-gray-500'>For any inquiries or support requests, feel free to reach out to us at <a href="mailto:saquib1312@gmail.com" className='text-white'>saquib1312@gmail.com</a>. We will get back to you as soon as possible.</p>
 
       <div className="flex justify-center gap-8 py-8">
-        <a href="https://www.linkedin.com/in/shahareyarkhan/" target="_blank" rel="noopener noreferrer" className="bg-white p-1 rounded-full">
+        <a href="https://www.linkedin.com/in/md-saquib-anjum-khan-414510254/" target="_blank" rel="noopener noreferrer" className="bg-white p-1 rounded-full">
           <div className="p-1 social-icons">
             <FaLinkedin className="text-3xl  text-blue-700 hover:text-blue-600 transition duration-300" />
           </div>
         </a>
-        <a href="https://www.instagram.com/_shahareyar_/" target="_blank" rel="noopener noreferrer" className="bg-white p-1 rounded-full">
+        {/* <a href="https://www.instagram.com/_shahareyar_/" target="_blank" rel="noopener noreferrer" className="bg-white p-1 rounded-full">
           <div className="p-1 social-icons">
             <FaInstagram className="text-3xl text-pink-600 hover:text-pink-500 transition duration-300" />
           </div>
-        </a>
-        <a href="https://x.com/shahareyar2003" target="_blank" rel="noopener noreferrer" className="bg-white p-1 rounded-full">
+        </a> */}
+        {/* <a href="https://x.com/shahareyar2003" target="_blank" rel="noopener noreferrer" className="bg-white p-1 rounded-full">
           <div className="p-1 social-icons">
             <FaTwitter className="text-3xl text-cyan-500 hover:text-cyan-400 transition duration-300" />
           </div>
-        </a>
-        <a href="https://shahareyar-anjum.vercel.app/" target="_blank" rel="noopener noreferrer" className="bg-white p-1 rounded-full">
+        </a> */}
+        <a href="https://portfolio-two-kappa-1a6k6lsj6q.vercel.app/" target="_blank" rel="noopener noreferrer" className="bg-white p-1 rounded-full">
           <div className="p-1 social-icons">
             <CgWebsite className="text-3xl text-gray-700 hover:text-gray-800 transition duration-300" />
           </div>
